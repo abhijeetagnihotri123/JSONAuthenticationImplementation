6e307c7348eb77f2bfc60c8bfcfd05811aa505de
abhijeetagnihotri123 <abhijeetagnihotri5@gmail.com>
2025-09-06 21:18:17 +0530

    Adding project

---
+++ HUMVEE/src/main/java/com/ODIN/HUMVEE/HumveeApplication.java
@@ -0,0 +1,13 @@
+package com.ODIN.HUMVEE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumveeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumveeApplication.class, args);
+	}
+
+}
