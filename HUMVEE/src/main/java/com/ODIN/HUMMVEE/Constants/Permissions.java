ffa66c47eaeaf16a2832526d26b1a13e8c303537
abhijeetagnihotri123 <abhijeetagnihotri5@gmail.com>
2025-09-06 21:23:31 +0530

    removing dumb code

--- HUMVEE/src/main/java/com/ODIN/HUMMVEE/Constants/Permissions.java
+++
@@ -1,9 +0,0 @@
-package com.ODIN.HUMMVEE.Constants;
-
-public enum Permissions {
-
-    GADGET_READ,
-    GADGET_WRITE,
-    GADGET_DELETE;
-
-}
--- HUMVEE/src/main/java/com/ODIN/HUMMVEE/Repositories/GadgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ODIN.HUMMVEE.Repositories;
-
-import com.ODIN.HUMMVEE.Entities.Gadget;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GadgetRepository extends JpaRepository<Gadget,Long> {
-}
